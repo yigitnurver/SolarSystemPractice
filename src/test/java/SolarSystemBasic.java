68b856f35c460ef91dcceb925e5e6dcd581c0cfc
yigitnurver <yigitnurver643@gmail.com>
2024-11-18 17:24:27 -0500

    solar system basic structure is ready

---
+++ src/test/java/SolarSystemBasic.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class SolarSystemBasic {
+    public static void main(String[] args) {
+        BasicStructure BB= new BasicStructure();
+        BB.Planets="2";
+        BB.Moon="2";
+        BB.Stars="10";
+        BB.Sun="1";
+        System.out.println("solar system is ready");
+    }
+}
